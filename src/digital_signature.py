4c4660bc5a6d435f17550b6718fd00cd24d6b911
joshbp <joshbp@brandeis.edu>
2024-04-27 19:38:06 -0400

    add functions for digital signatures

---
+++ src/digital_signature.py
@@ -0,0 +1,103 @@
+from cryptography.exceptions import InvalidSignature
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import padding, rsa
+
+from pathlib import Path
+
+
+class SignedFile:
+    """The contents (bytes) of a file and operations to sign and verify it
+    using RSA keys."""
+
+    def __init__(
+        self,
+        fp: Path,
+        algorithm: hashes.HashAlgorithm = hashes.SHA256()
+    ):
+        self._data = read_bytes(fp)
+        self.algorithm = algorithm
+        self.padding = padding.PSS(
+            mgf=padding.MGF1(algorithm=self.algorithm),
+            salt_length=padding.PSS.MAX_LENGTH
+        )
+
+    @property
+    def data(self):
+        return self._data
+
+    def sign(self, private_key: rsa.RSAPrivateKey) -> bytes:
+        """Cryptographically signs the data using a private RSA key."""
+        return private_key.sign(
+            self.data,
+            self.padding,
+            self.algorithm
+        )
+
+    def verify(self, public_key: rsa.RSAPublicKey, signature: bytes):
+        """Verifies the signature for the data using the RSA public key."""
+        try:
+            public_key.verify(
+                signature,
+                self.data,
+                self.padding,
+                self.algorithm
+            )
+            return True
+        except InvalidSignature:
+            return False
+
+
+def gen_keypair(
+    public_exponent: int = 65537,
+    key_size: int = 2048
+) -> tuple[rsa.RSAPublicKey, rsa.RSAPrivateKeyWithSerialization]:
+    """Generates a random RSA public/private key pair."""
+    private_key = rsa.generate_private_key(
+        public_exponent=public_exponent,
+        key_size=key_size
+    )
+    public_key = private_key.public_key()
+    return public_key, private_key
+
+
+def read_bytes(fp: Path):
+    """Reads the bytes contained in the given file."""
+    with fp.open('rb') as f:
+        return f.read()
+
+
+def read_public_key(path: Path) -> rsa.RSAPublicKey:
+    """Reads a file containing an RSA public key."""
+    with path.open('rb') as f:
+        return serialization.load_pem_public_key(f.read())
+
+
+def write_public_key(path: Path, key: rsa.RSAPublicKey):
+    """Serializes and writes an RSA public key."""
+    pem = key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+    with path.open('wb') as keyfile:
+        keyfile.write(pem)
+
+
+def read_private_key(path: Path, password: bytes) -> rsa.RSAPrivateKey:
+    """Reads a password protected file containing an RSA private key."""
+    with path.open('rb') as f:
+        return serialization.load_pem_private_key(f.read(), password=password)
+
+
+def write_private_key(
+    path: Path,
+    key: rsa.RSAPrivateKeyWithSerialization,
+    password: bytes
+):
+    """Serializes and writes an RSA private key."""
+    pem = key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.BestAvailableEncryption(password)
+    )
+    with path.open('wb') as keyfile:
+        keyfile.write(pem)
